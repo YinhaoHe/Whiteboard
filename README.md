075fa0856e84e505f6cbe482a7cef1322728536c
YinhaoHe <yinhaoh@uci.edu>
2020-09-02 14:51:46 -0700

    thickness button

--- README.md
+++ README.md
@@ -1,41 +1,41 @@
-## `Whiteboard` 1.0
+# MileStone 1
 
-## [Try it](https://yinhaohe.github.io/Whiteboard/)
+Version 1
 
-Author: Yinhao He
+# There are two drawing options:
 
-Version: 1.0
+1. Pencil
 
-***
+2. Rectangle
 
-### Features: 
+# There are four color options:
 
-- Pen
-- Eraser
-- Clear screen
-- Take a screenshot and download
-- Give feedback to the author
-![image](./demo/whiteboard1.0.jpg)
+1. black (default)
 
-### User Manual:
+2. red
 
-- Pen: 
-  - Left click pen button and hold leftclick to start draw.
-  - Left click color button on the top right to change color.
-  - Select different width of pen by clicking the pen width indicators on the right.
-- Eraser: 
-  - Left click eraser button then hold leftclick to earse the lines on the whiteboard.
-- Clear:
-  - Left click clear button to completely clean the whiteboard.
-  - Be aware clear cannot be reversed.
-- Screenshot:
-  - Left click screenshot button, then your browser will automatically download your whiteboard and save as a picture at your brower's default download location in your machine(You can check the setting in your perticular browser to see the default download location).
-  - Screenshot file name will be the date of the day you hit the screenshot button.
-- Feedback:
-  - Left click feedback button, and you will be redrict to author's github page.
-  - You are very welcomed to start a pull request.
+3. yellow
 
-### To do lists:
+4. blue
 
-- Add pop up page as user manual
-- Add color selector for users to have more color choices
+(can easily modify the code to add more colors)
+
+# Eraser: 
+
+there are three sizes for the eraser
+
+# Erase all
+
+working
+
+# Undo function: 
+
+can only undo one step backward 
+
+=> may need an array to store all previous canvas states
+
+# Rectangle small problem: 
+
+when drawing, can't display rectangle border as it changes 
+
+=> may need a second layer canvas to fix this
--- script.js
+++ script.js
@@ -1,124 +1,307 @@
-//Initial letiables
-let status = 'draw'; //'draw' 'clear'
-let dotWidth = 50;
-let color = 'DarkKhaki';
-let lineWidth = 5;
-let canvas = {
-  //Initial Canvas
-  init() {
-    this.ele = document.createElement('canvas');
-    document.body.appendChild(this.ele);
-    this.ctx = this.ele.getContext('2d');
-    //Background
-    this.floor = document.createElement('canvas');
-    this.floor.id = 'bg';
-    document.body.appendChild(this.floor);
-    this.floorCtx = this.floor.getContext('2d');
-    //Set size for canvas
-    this.width = this.ele.width = this.floor.width = window.innerWidth;
-    this.height = this.ele.height = this.floor.height = window.innerHeight;
+// first, make sure the canvas has right size
+window.addEventListener('resize', resizeCanvas);
+window.addEventListener('load', resizeCanvas);
 
-    return this;
-  },
-  get() {
-    return this;
-  },
-  /* Load background (futher use)
-  drawImage(imgPath) {
-    let that = this;
-    let img = new Image();
-    img.src = imgPath;
-    img.onload = function() {
-      that.floorCtx.clearRect(0, 0, that.width, that.height);
-      that.floorCtx.drawImage(img, that.width / 2 - 500, that.height / 2 - 100);
-    }
+const c = document.getElementById("canvas");
+const ctx = c.getContext("2d");
+let temp_canvas = document.createElement('canvas');
+let context = temp_canvas.getContext('2d');
 
-  }, */
-  //Bind mouseclick to function
-  bind() {
-    let ctx = this.ctx;
-    let startDraw = false;
-    this.ele.onmousedown = function(ev) {
-      startDraw = true;
-      let x = ev.clientX,
-        y = ev.clientY;
+// default drawing tool is pencil
+let tool = new tool_pencil();
+highlight('pencil');
+c.addEventListener('mousedown', draw_handler, false);
+c.addEventListener('mousemove', draw_handler, false);
+c.addEventListener('mouseup',   draw_handler, false);
+c.addEventListener('mouseout',  draw_handler, false);
+
+// backup canvas - for undo operation
+let back1 = document.createElement("canvas");
+let backCtx1 = back1.getContext("2d");
+let back2 = document.createElement("canvas");
+let backCtx2 = back2.getContext("2d");
+let restored = false;
+
+// pencil drawing tool
+function tool_pencil () {
+  var pencil = this;
+  this.started = false;
+
+  // This is called when you start holding down the mouse button.
+  // This starts the pencil drawing.
+  this.mousedown = function (e) {
       ctx.beginPath();
-    }
-    this.ele.onmousemove = function(ev) {
-      if (startDraw) {
-        console.log(status);
-        let x = ev.clientX,
-          y = ev.clientY;
-        if (status == 'draw') {
-          ctx.strokeStyle = color;
-          ctx.lineWidth = lineWidth;
-          ctx.lineTo(x, y);
-          ctx.stroke();
-        } else if (status == 'clear') {
-          ctx.strokeStyle = 'rgba(0,0,0,1)';
-          ctx.clearRect(x - 20, y - 20, 40, 40);
-        }
-      }
-    }
-    this.ele.onmouseup = function() {
-      ctx.closePath();
-      startDraw = false;
-    }
-  }
-}
-canvas.init().bind();
+      ctx.moveTo(e._x, e._y);
+      pencil.started = true;
+  };
 
-//Change color function
-function changeColor(c) {
-  color = c;
-}
-//Draw function
-function draw() {
-  console.log('You hit draw function');
-  status = 'draw';
-}
-//Eraser function
-function eraser() {
-  console.log('You hit eraser function');
-  status = 'clear';
-}
-//Change pen width
-function changeWidth(w) {
-  lineWidth = w;
+  // This function is called every time you move the mouse. Obviously, it only 
+  // draws if the tool.started state is set to true (when you are holding down 
+  // the mouse button).
+  this.mousemove = function (e) {
+    if (pencil.started) {
+      ctx.lineTo(e._x, e._y);
+      ctx.stroke();
+    }
+  };
+
+  // This is called when you release the mouse button.
+  this.mouseup = function (e) {
+    if (pencil.started) {
+      pencil.mousemove(e);
+      pencil.started = false;
+      saveCanvas();
+    }
+  };
+
+    // This is called when you move the mouse button out.
+  this.mouseout = function (e) {
+    if (pencil.started) {
+      pencil.mousemove(e);
+      pencil.started = false;
+      saveCanvas();
+    }
+  };
 }
 
-//Clear canvas
-function clearCanvas() {
-  let ins = canvas.get();
-  ins.ctx.clearRect(0, 0, ins.width, ins.height);
-}
-//Screenshot and download
-function screena() {
-  //Set pic type to png
-  let type = 'png';
-  let imgdata = canvas.get().ele.toDataURL(type);
-  //Change mime-type to image/octet-stream
-  //Force download
-  let fixtype = function(type) {
-    type = type.toLocaleLowerCase().replace(/jpg/i, 'jpeg');
-    let r = type.match(/png|jpeg|bmp|gif/)[0];
-    return 'image/' + r;
-  }
-  imgdata = imgdata.replace(fixtype(type), 'image/octet-stream')
-  //Download pic.png
-  let saveFile = function(data, filename) {
-    let link = document.createElement('a');
-    link.href = data;
-    link.download = filename;
-    let event = document.createEvent('MouseEvents');
-    event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    link.dispatchEvent(event);
-  }
-  let filename = new Date().toLocaleDateString() + '.' + type;
-  saveFile(imgdata, filename);
+// rectangle drawing tool
+function tool_rect() {
+	var rect = this;
+  this.started = false;
+  
+
+	this.mousedown = function (ev) {
+		rect.started = true;
+		rect.x0 = ev._x;
+		rect.y0 = ev._y;
+	};
+
+	this.mousemove = function (ev) {
+		if (!rect.started) {
+			return;
+		}
+
+		var x = Math.min(ev._x,	rect.x0),
+			y = Math.min(ev._y,	rect.y0),
+			w = Math.abs(ev._x - rect.x0),
+			h = Math.abs(ev._y - rect.y0);
+
+		context.clearRect(0, 0, c.width, c.height);
+		if (!w || !h) {
+			return;
+		}
+    context.lineWidth = 2;
+    context.strokeStyle = ctx.strokeStyle;
+		context.strokeRect(x, y, w, h);
+	};
+
+	this.mouseup = function (ev) {
+		if (rect.started) {
+			rect.mousemove(ev);
+      rect.started = false;
+      updateImg();
+      saveCanvas();
+		}
+  };
+  
+  this.mouseout = function (ev) {
+		if (rect.started) {
+			rect.mousemove(ev);
+      rect.started = false;
+      updateImg();
+      saveCanvas();
+		}
+	};
+};
+
+// eraser tool
+function tool_eraser (radius) {
+  var eraser = this;
+  this.started = false;
+  this.radius = radius
+  const color = ctx.strokeStyle;
+
+  this.mousedown = function (e) {
+      ctx.beginPath();
+      ctx.arc(e._x,e._y,radius,0,2*Math.PI);
+      ctx.strokeStyle = 'white';
+      ctx.fillStyle = 'white';
+      ctx.fill();
+      ctx.stroke();
+      eraser.started = true;
+  };
+
+  // This function is called every time you move the mouse. Obviously, it only 
+  // draws if the tool.started state is set to true (when you are holding down 
+  // the mouse button).
+  this.mousemove = function (e) {
+    if (eraser.started) {
+      ctx.moveTo(e._x, e._y);
+      ctx.arc(e._x,e._y,radius,0,2*Math.PI);
+      ctx.fill();
+      ctx.stroke();
+    }
+  };
+
+  // This is called when you release the mouse button.
+  this.mouseup = function (e) {
+    if (eraser.started) {
+      eraser.started = false;
+      ctx.strokeStyle = color;
+      saveCanvas();
+    }
+  };
+
+    // This is called when you move the mouse button out.
+  this.mouseout = function (e) {
+    if (eraser.started) {
+      eraser.started = false;
+      ctx.strokeStyle = color;
+      saveCanvas();
+    }
+  };
 }
 
-//Feedback to Yinhao's Github
-function feedback() {
-  window.location.href = "https://github.com/ArdentLabs/milestone-1-YinhaoHe";
+// General-purpose event handler
+function draw_handler (ev) {
+  if (ev.layerX || ev.layerX == 0) { // Firefox
+    ev._x = ev.layerX;
+    ev._y = ev.layerY;
+  } else if (ev.offsetX || ev.offsetX == 0) { // Opera
+    ev._x = ev.offsetX;
+    ev._y = ev.offsetY;
+  }
+
+  // Call the event handler of the tool.
+  var func = tool[ev.type];
+  if (func) {
+    func(ev);
+  }
+}
+
+// clear the canvas with previous states
+function resetCanvas(){
+  document.getElementById('color_option').style.display = "none";
+  document.getElementById('eraser_option').style.display = "none";
+  
+  ctx.clearRect(0,0,c.width,c.height);
+  temp_canvas.width = c.width;
+  temp_canvas.height = c.height;
+  back1 = document.createElement("canvas");
+  backCtx1 = back1.getContext("2d");
+  backCtx1.fillStyle = back1.fillStyle;
+  back2 = document.createElement("canvas");
+  backCtx2 = back2.getContext("2d");
+  backCtx2.fillStyle = back2.fillStyle;
+  back1.width = c.width;
+  back1.height = c.height;
+  back2.width = c.width;
+  back2.height = c.height;
+  restored = false;
+}
+
+// resize the canvas
+function resizeCanvas(e){
+  // adjust the size of canvas dynamically
+  const board = document.getElementById('board');
+  const cs = getComputedStyle(board);
+  c.width = parseInt(cs.getPropertyValue('width'), 10);
+  c.height = parseInt(cs.getPropertyValue('height'), 10)*0.8824;
+  resetCanvas();
+}
+
+// restore canvas to the previous state
+function restoreCanvas(){
+  document.getElementById('color_option').style.display = "none";
+  document.getElementById('eraser_option').style.display = "none";
+
+  if(!restored){
+    ctx.clearRect(0,0,c.width,c.height);
+    ctx.drawImage(back1,0,0);
+    const newbackCanvas = document.createElement("canvas");
+    const newbackCtx = newbackCanvas.getContext("2d");
+    newbackCanvas.width = c.width;
+    newbackCanvas.height = c.height;
+    newbackCtx.drawImage(c,0,0);
+    back2 = newbackCanvas;
+    restored = true;
+  }
+}
+
+// save current state
+function saveCanvas(){
+  const newbackCanvas = document.createElement("canvas");
+  const newbackCtx = newbackCanvas.getContext("2d");
+  newbackCanvas.width = c.width;
+  newbackCanvas.height = c.height;
+  newbackCtx.drawImage(c,0,0);
+  back1 = back2;
+  back2 = newbackCanvas;
+  restored = false;
+}
+
+// draw the background layer to ctx (used for rectangle)
+function updateImg(){
+  ctx.drawImage(temp_canvas,0,0);
+  context.clearRect(0,0,temp_canvas.width,temp_canvas.height);
+}
+
+// show color options
+function showColorOption(){
+  document.getElementById('eraser_option').style.display = "none";
+  if(document.getElementById('color_option').style.display == "block")
+    document.getElementById('color_option').style.display = "none";
+  else
+    document.getElementById('color_option').style.display = "block";
+}
+
+// show eraser options
+function showEraserOption(){
+  document.getElementById('color_option').style.display = "none";
+  if(document.getElementById('eraser_option').style.display == "flex")
+    document.getElementById('eraser_option').style.display = "none";
+  else
+    document.getElementById('eraser_option').style.display = "flex";
+}
+
+// change drawing color
+function changeColor(color){
+  ctx.strokeStyle = color;
+  context.strokeStyle = color;
+}
+
+// change the drawing tool to pencil
+function changePen(){
+  document.getElementById('color_option').style.display = "none";
+  document.getElementById('eraser_option').style.display = "none";
+  highlight('pencil');
+  tool = new tool_pencil();
+}
+
+// change the drawing tool to rect
+function changeRect(){
+  document.getElementById('color_option').style.display = "none";
+  document.getElementById('eraser_option').style.display = "none";
+  highlight('rect');
+  tool = new tool_rect();
+}
+
+// change the drawing tool to eraser
+function changeEraser(radius){
+  document.getElementById('color_option').style.display = "none";
+  highlight('eraser');
+  tool = new tool_eraser(radius);
+  return;
+}
+
+// highlight the selected drawing tool
+function highlight(e){
+  let pencil = document.getElementById("pencil");
+  let eraser = document.getElementById("eraser");
+  let rect = document.getElementById("rect");
+  pencil.classList.remove("hoverBtn");
+  eraser.classList.remove("hoverBtn");
+  rect.classList.remove("hoverBtn");
+  let div = document.getElementById(e);
+  div.classList.add("hoverBtn");
 }
